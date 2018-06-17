efad646da9e003a187061b93a120696a884c4967
Ozan192 <38989289+Ozan192@users.noreply.github.com>
2018-06-17 19:15:54 +0100

    Added .cpp and .h files from previous project

---
+++ PIDlibrary.cpp
@@ -0,0 +1,125 @@
+#if ARDUINO >= 100
+  #include "Arduino.h"
+#else
+  #include "WProgram.h"
+#endif
+
+#include "PIDlibrary.h"
+
+basePID::basePID(){}
+
+void basePID::setCoefficients(float p, float i, float d)
+{
+  this->Kp=p;
+  this->Ki=i*float(time_delta);
+  this->Kd=d/float(time_delta);
+}
+
+void basePID::setTimeDelta(unsigned long time_delta)
+{
+  Ki = Ki * float(time_delta / this->time_delta);
+  Kd = Kd * float(time_delta / this->time_delta);
+  this->time_delta = time_delta;
+}
+
+void basePID::integralSat(float ilimitlow, float ilitmithigh)
+{
+  this->ilimithigh = ilimithigh;
+  this->ilimitlow = ilimitlow;
+}
+
+void basePID::outputSat(float outputlimitlow, float outputlimithigh)
+{
+  this->outputlimithigh = outputlimithigh;
+  this->outputlimitlow = outputlimitlow;
+}
+
+void basePID::output(){};
+
+float basePID:: getPmode() {return Pmode;}
+float basePID:: getImode() {return Imode;}
+float basePID:: getDmode() {return Dmode;}
+float basePID:: getKp() {return Kp;}
+float basePID:: getKi() {return Ki;}
+float basePID:: getKd() {return Kd;}
+unsigned long  basePID::getTimeDelta() {return time_delta;}
+
+PID::PID(float kp, float ki, float kd, unsigned long int time_delta,
+    float* reference, float* feedback, float* controlout)
+    {
+      this->time_delta = time_delta;
+      this->reference = reference;
+      this->feedback = feedback;
+      this->controlout = controlout;
+      
+      setCoefficients(kp,ki,kd);
+      setTimeDelta(time_delta);
+      integralSat(1000,2000);
+      outputSat(1000,1000);
+      error_prev = 0;
+      time_prev = millis()-time_delta;
+    }
+
+void PID::output()
+{
+  if(millis()-time_prev >= time_delta)
+  {
+    error = *reference-*feedback;
+
+    Pmode = Kp*error;
+    Imode += Ki*error;
+    Dmode = Kd*(error-error_prev);
+    control = Pmode+Imode+Dmode;
+    
+    if(Imode > ilimithigh) Imode = ilimithigh;
+    if(Imode < ilimitlow) Imode = ilimitlow;
+    
+    if(control > outputlimithigh) *controlout = outputlimithigh;
+    else if( control < outputlimitlow) *controlout = outputlimitlow;
+    else *controlout = control;
+    
+    error_prev = error;
+    time_prev = millis();
+  }
+}
+
+PI_D::PI_D(float kp, float ki, float kd, unsigned long int time_delta,
+    float* reference, float* feedback, float* controlout)
+    {
+      this->time_delta = time_delta;
+      this->reference = reference;
+      this->feedback = feedback;
+      this->controlout = controlout;
+      
+      setCoefficients(kp,ki,kd);
+      setTimeDelta(time_delta);
+      integralSat(1000,2000);
+      outputSat(1000,1000);
+      feedback_prev = 0;
+      time_prev = millis()-time_delta;
+    }
+
+void PI_D::output()
+{
+  if(millis()-time_prev >= time_delta)
+  {
+    error = *reference-*feedback;
+
+    Pmode = Kp*error;
+    Imode += Ki*error;
+    Dmode = Kd*(*feedback-feedback_prev);
+    control = Pmode+Imode+Dmode;
+    
+    if(Imode > ilimithigh) Imode = ilimithigh;
+    if(Imode < ilimitlow) Imode = ilimitlow;
+    
+    if(control > outputlimithigh) *controlout = outputlimithigh;
+    else if( control < outputlimitlow) *controlout = outputlimitlow;
+    else *controlout = control;
+    
+    feedback_prev = *feedback;
+    time_prev = millis();
+  }
+}
+
+
---
+++ PIDlibrary.h
@@ -0,0 +1,79 @@
+class basePID
+{
+  public:
+
+    virtual void setCoefficients(float, float, float);
+    
+    virtual void setTimeDelta(unsigned long);
+
+    virtual void integralSat(float, float);
+
+    virtual void outputSat(float, float);
+    
+    virtual void output()=0;
+
+    virtual float getPmode();
+    virtual float getImode();
+    virtual float getDmode();
+    virtual float getKp();
+    virtual float getKi();
+    virtual float getKd();
+    
+    virtual unsigned long getTimeDelta();
+  
+  
+  protected:
+  
+    float Kp;
+    float Ki;
+    float Kd;
+    float Pmode;
+    float Imode;
+    float Dmode;
+    
+    float error;
+    float errorDt;
+    float error_prev;
+    float feedback_prev;
+    float integral;
+
+    float control;
+    float* reference;
+    float* feedback;
+    float* controlout;
+    
+    unsigned long int time_prev;
+    unsigned long int time_delta;
+
+    float ilimithigh;
+    float ilimitlow;
+    float outputlimithigh;
+    float outputlimitlow;
+    
+    basePID();
+  
+};
+
+class PID : public basePID
+{
+  public:
+
+    PID(float, float, float, unsigned long int,
+        float*, float*, float*);
+    
+    void output();
+
+  
+};
+
+class PI_D : public basePID
+{
+  public:
+
+    PI_D(float, float, float, unsigned long int,
+        float*, float*, float*);
+    
+    void output();
+
+  
+};
